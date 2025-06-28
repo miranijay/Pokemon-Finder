39e6d2f5e0a1ef5b59ba821b25c636dbcca23127
miranijay <miranijay188@gmail.com>
2025-06-28 20:56:48 +0530

    update options

--- src/Component/PokemonCard.jsx
+++ src/Component/PokemonCard.jsx
@@ -90,18 +90,18 @@ const PokemonCard = () => {
                                         onChange={handleSelectChange}
                                     >
                                         <option value="all">All</option>
-                                        <option value="grass">Grass</option>
-                                        <option value="fire">Fire</option>
-                                        <option value="electric">Electric</option>
-                                        <option value="ghost">Ghost</option>
-                                        <option value="normal">Normal</option>
                                         <option value="bug">Bug</option>
-                                        <option value="water">Water</option>
-                                        <option value="poison">Poison</option>
-                                        <option value="fighting">Fighting</option>
-                                        <option value="psychic">Psychic</option>
                                         <option value="dragon">Dragon</option>
                                         <option value="dark">Dark</option>
+                                        <option value="electric">Electric</option>
+                                        <option value="fire">Fire</option>
+                                        <option value="fighting">Fighting</option>
+                                        <option value="grass">Grass</option>
+                                        <option value="ghost">Ghost</option>
+                                        <option value="normal">Normal</option>
+                                        <option value="poison">Poison</option>
+                                        <option value="psychic">Psychic</option>
+                                        <option value="water">Water</option>
                                     </select>
                                 </div>
 
